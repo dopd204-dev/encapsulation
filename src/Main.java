ee7750f5ae7f7b289b0ba2963d885d7e4345b1d1
Талант <dop204@gmail.con>
2026-01-28 14:12:40 +0300

    cod

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            Person person1 = new Person();
+            person1.fio = "Абдижалилов Талант";
+            person1.age = 17;
+            person1.grade = "M";
+            person1.nation = "Кыргыз";
+            person1.profession = "Java";
+            person1.holostoi = true;
+
+        }
+    }
