01e39c8e86133314570e2f9c64a57c2543a192c0
Талант <dop204@gmail.con>
2026-01-29 18:07:01 +0300

    cod

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,5 @@
 public class Car {
-    String model;
-    String tsvet;
-    int jyly;
+
 
     }
 
--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,6 @@
 public class Person {
-        String fio;
-        int age;
-        String nation;
-        String grade;
-        String profession;
-        boolean holostoi;
+}
 
-        public  String rum(){
-            return this.fio + "может бегать";
-        }
-        public String speak(){
-            return this.fio + "может разговаривать";
 
-        }
-    }
+
 
