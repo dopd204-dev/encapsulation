63356b02b67f7ca9e7fe7ca41f067dfec552fe6e
Талант <dop204@gmail.con>
2026-02-02 14:17:15 +0300

    cod

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static boolean contains(int a, int... varargs) {
-        for (int number : varargs) {
-            if (number == a) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public static void main(String[] args) {
-
-        System.out.println(contains(5, 1, 2, 3, 4, 5));
-        System.out.println(contains(10, 1, 2, 3, 4, 5));
-        System.out.println(contains(3, 3, 6, 7, 8));
-        System.out.println(contains(2,3,5,6,7));
-    }
 }
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.setLength(5);
+        rectangle.setWidth(4);
+
+        double area = rectangle.calculateArea();
+
+        System.out.println("Тик бурчтуктун аянты: " + area);
+
+    }
+
 
 
 
