306214ea007cd631fcf719b9d430ecbe03a4a794
Талант <dop204@gmail.con>
2026-02-02 15:22:19 +0300

    cod

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,62 @@
 public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private int speed;
 
-
+    public Car(String brand, String model, int year, int speed) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.speed = speed;
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed >= 0) {
+            this.speed = speed;
+        }
+    }
+
+    public void increaseSpeed(int value) {
+        this.speed += value;
+    }
+
+    public void decreaseSpeed(int value) {
+        this.speed -= value;
+        if (this.speed < 0) {
+            this.speed = 0;
+        }
+    }
+}
+
+
+
 
