d1cb5d64e448edcc07c93c229f3b222d9d824107
Талант <dop204@gmail.con>
2026-02-02 13:23:06 +0300

    cod

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static boolean contains(int a, int... varargs) {
+        for (int number : varargs) {
+            if (number == a) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        User user1 = new User("Иван", 20, "ivan@mail.com");
-        User user2 = new User("Анна", 18, "anna@mail.com");
 
-        user1.printInfo();
-        user2.printInfo();
+        System.out.println(contains(5, 1, 2, 3, 4, 5));
+        System.out.println(contains(10, 1, 2, 3, 4, 5));
+        System.out.println(contains(3, 3, 6, 7, 8));
+        System.out.println(contains(2,3,5,6,7));
     }
 }
+
+
 
 
 
