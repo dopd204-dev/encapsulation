e7b5ea436e4de15bf1fb7e1a38ec7d5a3ce53c90
Талант <dop204@gmail.con>
2026-02-02 14:43:27 +0300

    cod

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,42 @@
 public class Main {
 }
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
 
-        rectangle.setLength(5);
-        rectangle.setWidth(4);
+        Scanner scanner = new Scanner(System.in);
+        Student student = new Student();
 
-        double area = rectangle.calculateArea();
+        System.out.print("Атыңызды жазыңыз: ");
+        student.setName(scanner.nextLine());
 
-        System.out.println("Тик бурчтуктун аянты: " + area);
+        System.out.print("Фамилияңызды жазыңыз: ");
+        student.setSurname(scanner.nextLine());
 
+        System.out.print("Жашыңызды жазыңыз: ");
+        student.setAge(scanner.nextInt());
+        scanner.nextLine();
+
+        List<String> courses = new ArrayList<>();
+        System.out.print("Канча курс окуйсуз? ");
+        int courseCount = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < courseCount; i++) {
+            System.out.print((i + 1) + "-курс: ");
+            courses.add(scanner.nextLine());
+        }
+
+        student.setCourses(courses);
+
+        System.out.println("\n--- Студенттин маалыматы ---");
+        System.out.println("Аты: " + student.getName());
+        System.out.println("Фамилиясы: " + student.getSurname());
+        System.out.println("Жашы: " + student.getAge());
+        System.out.println("Курстары:");
+        for (String course : student.getCourses()) {
+            System.out.println("- " + course);
+        }
     }
+
 
 
 
---
+++ src/Student.java
@@ -0,0 +1,49 @@
+import java.util.List;
+
+class Student {
+    private String name;
+    private String surname;
+    private int age;
+    private List<String> courses;
+
+    // Getter жана Setterлер
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<String> courses) {
+        this.courses = courses;
+    }
+}
+
+
+
+
+
+
+
+
